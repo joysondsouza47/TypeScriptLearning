6c4cc7995d67f9c8c8d29c7df73a16eedf0e7ad4
joysondsouza47 <joysondsouza47@gmail.com>
2026-06-03 08:28:16 +0530

    03_05_sixthcommit

--- Day2/Datatypes.ts
+++ Day2/Datatypes.ts
@@ -14,9 +14,25 @@
  
 //     TypeScript warns you about potential bugs early.
 
-const result = "4" + 5;
-console.log(result);            
+// const result = "4" + 5;
+// console.log(result);            
 
 // const result:number = "4" + 5;     only when datatype is applied the type safety works
 // console.log(result);             // Error: Type 'string' is not assignable to type 'number' 
 // this feature is available only in typescript in java we cannot assign the data types manually
+
+
+//  Type Annotation
+//  Type Inference
+
+//  Type Annotation -- explicitly telling the type of variable.
+//  example ---- :number  :boolean  :string
+
+//  Type Inferrence -- automatically guesses the type if you don’t annotate it
+//  let message = "Hello";       // TypeScript infers `message` as `string` 
+//  let count = 42;              // TypeScript infers `count` as `number` 
+//  message = 123;               // Error (TypeScript knows `message` must stay a string) 
+
+// Key Differences: 
+// • Type Annotation: You manually define the type. 
+// • Type Inference: TypeScript intelligently figures it out. 
