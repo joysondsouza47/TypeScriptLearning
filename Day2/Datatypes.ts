54669b890c97e99efe3664502aef4d0144409c07
joysondsouza47 <joysondsouza47@gmail.com>
2026-06-03 08:21:33 +0530

    03_05_fourthcommit

---
+++ Day2/Datatypes.ts
@@ -0,0 +1,22 @@
+// Dynamic typed -  checked at runtime
+// Statically typed -  checked at compile time
+
+//JavaScript is Not Type-Safe 
+//    const result = "5" + 3; // JavaScript converts 3 to string 
+//    console.log(result);    // Output: "53" (not 8) 
+ 
+//    No error, but the result may not be what you intended. 
+
+//   TypeScript is Type-Safe 
+// • TypeScript prevents operations between incompatible types. 
+// • Example: 
+//     const result: number = "5" + 3;  //Error: Type 'string' is not assignable to type 'number' 
+ 
+//     TypeScript warns you about potential bugs early.
+
+const result = "4" + 5;
+console.log(result);            
+
+// const result:number = "4" + 5;     only when datatype is applied the type safety works
+// console.log(result);             // Error: Type 'string' is not assignable to type 'number' 
+// this feature is available only in typescript in java we cannot assign the data types manually
---
+++ Day2/typesafety.js
@@ -0,0 +1,5 @@
+// const result:number = "3" + 5;
+// console.log(result);
+
+
+// this feature is available only in typescript in java we cannot assign the data types manually
